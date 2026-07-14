2886e2035f79c5c84af80d986458d73f6bd5670d
Lucián Kučera <kucerl12@fit.cvut.cz>
2026-07-14 16:11:15 +0200

    init

---
+++ include/hpc_stdlib/queue.h
@@ -0,0 +1,20 @@
+#pragma once
+
+template <typename T>
+class Queue
+{
+public:
+    Queue()
+    {
+    }
+
+    ~Queue()
+    {
+    }
+
+    Queue(const Queue<T> &other)
+    {
+    }
+
+private:
+};
